d976fe0024f2a12046917fc607a1cd5bee25ec6e
楊崇遠 <tony@yangchongyuande-MacBook-Pro.local>
2020-04-29 17:22:50 +0800

    add ReadMe

---
+++ README.md
@@ -0,0 +1,3 @@
+# Firebase Messaging Test for Chat Room
+
+主要利用 FCM 推播功能去實現簡易聊天室功能
--- app/src/main/java/com/test/testfcmmessaging/MainActivity.kt
+++ app/src/main/java/com/test/testfcmmessaging/MainActivity.kt
@@ -17,6 +17,7 @@ import com.google.firebase.FirebaseApp
 import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.iid.InstanceIdResult
 import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.RemoteMessage
 import kotlinx.android.synthetic.main.activity_main.*
 
 
